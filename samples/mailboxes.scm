b2ca3cb1703f7913d062c5ac776428a02088bb8e
Omar Jarjur <procrastitron@users.sourceforge.net>
2014-12-14 14:43:26 -0800

    Changed the mailboxes sample to use syntax rules for the mailbox-full? and mailbox-empty? predicates

--- samples/mailboxes.scm
+++ samples/mailboxes.scm
@@ -12,41 +12,43 @@
 (define (make-mailbox . args)
   (let ((size (if (pair? args) (car args) 1)))
     (make-mailbox-record (make-vector size) size 0 0)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; TODO: Change the mailbox-empty? and mailbox-full? predicates to           ;;
-;; be syntax rules, so that they can be included as part of an atomic block. ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define (mailbox-empty? mailbox)
-  (= (mailbox-read-position mailbox)
-     (mailbox-insert-position mailbox)))
-(define (mailbox-full? mailbox)
-  (= (- (mailbox-insert-position mailbox)
-        (mailbox-size mailbox))
-     (mailbox-read-position mailbox)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Note: the mailbox-empty? and mailbox-full? predicates are syntax rules, ;;
+;; so that they can be included as part of an atomic block.                ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define-syntax mailbox-empty?
+  (syntax-rules ()
+    ((mailbox-empty? mailbox)
+     (= (mailbox-read-position mailbox)
+        (mailbox-insert-position mailbox)))))
+(define-syntax mailbox-full?
+  (syntax-rules ()
+    ((mailbox-full? mailbox)
+     (= (- (mailbox-insert-position mailbox)
+           (mailbox-size mailbox))
+        (mailbox-read-position mailbox)))))
 (define (mailbox-send mailbox message)
-  (let ((store (mailbox-store mailbox))
-        (read-position (mailbox-read-position mailbox))
-        (insert-position (mailbox-insert-position mailbox))
-        (size (mailbox-size mailbox)))
-    (if (= (- insert-position size) read-position)
-        ;; The fixed size buffer is full. Wait and check again.
-        (mailbox-send mailbox message)
-        ;; Send a message, the entire following block is atomic,
-        ;; so we don't have to worry about read-modify-write conflicts.
+  (if (mailbox-full? mailbox)
+      ;; The fixed size buffer is full. Wait and check again.
+      (mailbox-send mailbox message)
+      ;; Send a message, this method's entire body is atomic,
+      ;; so we don't have to worry about read-modify-write conflicts.
+      (let ((store (mailbox-store mailbox))
+            (insert-position (mailbox-insert-position mailbox))
+            (size (mailbox-size mailbox)))
         (begin (set-mailbox-insert-position! mailbox (+ insert-position 1))
                (vector-set! store (remainder insert-position size) message)
                mailbox))))
 (define (mailbox-poll mailbox message-handler empty-thunk)
-  (let ((store (mailbox-store mailbox))
-        (read-position (mailbox-read-position mailbox))
-        (insert-position (mailbox-insert-position mailbox))
-        (size (mailbox-size mailbox)))
-    (if (= read-position insert-position)
-        ;; The fixed size buffer is empty.
-        (empty-thunk)
-        ;; Send a message to the handler. The entire following block (until
-        ;; the final tail-call) is atomic, so we don't have to worry about
-        ;; read-modify-write conflicts.
+  (if (mailbox-empty? mailbox)
+      ;; The fixed size buffer is empty.
+      (empty-thunk)
+      ;; Send a message to the handler. Other than the final tail-call,
+      ;; this method's entire body is atomic, so we don't have to worry
+      ;; about read-modify-write conflicts.
+      (let ((store (mailbox-store mailbox))
+            (read-position (mailbox-read-position mailbox))
+            (size (mailbox-size mailbox)))
         (begin (set-mailbox-read-position! mailbox (+ read-position 1))
                (message-handler (vector-ref store
                                             (remainder read-position size)))))))
